aa658c182d8ba009b4deeb99a1570991752b30df
Sam Lichlyter <slichlyter12@me.com>
2016-06-20 16:23:01 -0700

    Initial Commit

---
+++ ResearchKit Example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ResearchKit Example
+//
+//  Created by Samuel Lichlyter on 6/20/16.
+//  Copyright © 2016 Samuel Lichlyter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
